547b8b4696ed0693895180f2edc71a37f9fb388d
ARTY-PC\Arty <arthurnangai@live.com>
2018-08-17 19:24:36 +0300

    question class modified to have user_id attribute not id attribute.  Changes to be committed: 	modified:   app/question.py

--- app/question.py
+++ app/question.py
@@ -1,22 +1,25 @@
 from datetime import datetime
+import json
 
 class Question:
 
-    def __init__(self, id, detail, author):
-        self.id = id
+    def __init__(self, user_id, detail, author):
+        self.user_id = user_id
         self.detail = detail
         self.author = author
         self.answers = list() 
-        self.date = datetime.now()
+        self.date = str(datetime.now())
 
 questions = {}
 question1 = Question(1, "how to log", "arthur nangai")
 no = len(questions) + 1
-questions[no] = question1
+questions[no] = question1.__dict__
 
 question2 = Question(2, "how to bog", "arthur bark")
 no = len(questions) + 1
-questions[no] = question2
+questions[no] = question2.__dict__
 
-for x in questions:
-    print("author is %s date is %s" % (questions[x].author, questions[x].date))
+print(questions)
+output = json.dumps(questions)
+print(output)
+##   print("author is %s date is %s" % (questions[x].author, questions[x].date))

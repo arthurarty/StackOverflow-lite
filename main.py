7f5fdbf191b927d12f36de72d5a7a1b6f148385e
ARTY-PC\Arty <arthurnangai@live.com>
2018-08-18 14:44:23 +0300

    add question functionality successfully added. to add question, post method to /questions Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, json
 from app.question import Question
 app = Flask(__name__)
 
@@ -6,35 +6,29 @@ questions = {}
 
 question1 = Question(1, "how to log", "arthur nangai")
 no = len(questions) + 1
-questions[no] = question1
+questions[no] = question1.__dict__
 
 question2 = Question(2, "how to bog", "arthur bark")
 no = len(questions) + 1
-questions[no] = question2
+questions[no] = question2.__dict__
 
-@app.route('/')
-def hello_world():
-    question = Question(8, "Hello there", "arthur")
-    return str(question.id)
-    #return 'Hello, World!'
 
 @app.route('/v1/questions' ,methods=['GET', 'POST'])
 def fetch_all_questions():
     if request.method == 'POST':
-        pass
-    else:
-        text = ""
-        for x in questions:
-            text = text + ("author is %s date is %s \n" % (questions[x].author, questions[x].date)) 
-    
-        return text
-    #return 'Hello, World!'
+        newQuestion = Question(2, request.form['detail'], request.form['author'])
+        no = len(questions) + 1
+        questions[no] = newQuestion.__dict__
+        output = json.dumps(questions[no])
+        return output
+    else: 
+        output = json.dumps(questions)
+        return output
 
 @app.route('/v1/questions/<int:question_id>/')
 def fetch_single_question(question_id):
-    text = ""
-    text = text + ("author is %s date is %s \n" % (questions[question_id].author, questions[question_id].date)) 
-    return text
+    output = json.dumps(questions[question_id])
+    return output
 
 if __name__ == "__main__":
     app.run()

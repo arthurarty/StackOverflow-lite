d9d3f2cccf3ca451a128e0d0e94f7bd3ffc9d06c
ARTY-PC\Arty <arthurnangai@live.com>
2018-08-21 20:31:35 +0300

    public getter methods added Changes to be committed: 	modified:   app/answer.py

--- app/answer.py
+++ app/answer.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+"""Class Answer represents an answer to a question
+"""
 class Answer: 
 
     def __init__(self, question_id, body, author):
@@ -7,3 +9,15 @@ class Answer:
         self.body = body
         self.author = author
         self.date = str(datetime.now())
+
+    def return_question_id(self):
+        return self.question_id
+
+    def return_answer_author(self):
+        return self.author
+
+    def return_answer_date(self):
+        return self.date
+
+    def return_answer_body(self):
+        return self.body

d8c83ce275e9df4ff2667c5b9f2db277ba7338d1
ARTY-PC\Arty <arthurnangai@live.com>
2018-08-24 12:03:26 +0300

    useless texts have been removed. Changes to be committed: 	modified:   tests/test_answer.py 	modified:   tests/test_questions.py

--- tests/test_answer.py
+++ tests/test_answer.py
@@ -5,15 +5,4 @@ def test_is_instance_of_answer():
     new_answer = Answer(5, "Hello there", "Nangai")
     assert isinstance(new_answer, Answer)
 
-def test_has_attribute_id():
-    new_answer = Answer(7, "Login in by", "Nangai")
-    assert hasattr(new_answer, "question_id")
-
-def test_has_attribute_author():
-    new_answer = Answer(7, "Login in by", "Nangai")
-    assert hasattr(new_answer, "author")
-
-def test_has_attribute_date():
-    new_answer = Answer(7, "Log in by", "Nangai")
-    assert hasattr(new_answer, "date")
     
--- tests/test_questions.py
+++ tests/test_questions.py
@@ -5,19 +5,3 @@ import pytest
 def test_is_instance_of_question():
     new_question = Question(5, "Hello there", "Nangai")
     assert isinstance(new_question, Question)
-
-def test_has_attribute_id():
-    new_question = Question(7, "How to login", "Nangai")
-    assert hasattr(new_question, "user_id")
-
-def test_has_attribute_author():
-    new_question = Question(7, "How to login", "Nangai")
-    assert hasattr(new_question, "author")
-
-def test_has_attribute_date():
-    new_question = Question(7, "How to login", "Nangai")
-    assert hasattr(new_question, "date")
-
-def test_has_attribute_list():
-    new_question = Question(7, "How to login", "Nangai")
-    assert hasattr(new_question, "answers")
